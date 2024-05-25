1a044a2df76a3bafe4a54a7afe88670ca22fd68f
nidhisharma63 <nidhisharma639593@gmail.com>
2024-05-25 11:49:49 +0530

    section2 defined

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import LandingPage from "./components/LandingPage";
+import Section2 from "./components/Section2";
 
 const App = () => {
   return (
     <div>
       <LandingPage />
+      <Section2 />
     </div>
   );
 };
---
+++ src/components/Section2.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Section2 = () => {
+  return (
+    <div className="h-[700px] flex border-2 bg-[#fccff6] text-[#373637] align-middle justify-between p-[4rem] pt-[8rem]">
+      {/* <div className="w-[80%] h-[80%] m-auto border-2 flex gap-24"> */}
+      <div className="">
+        <p className="text-4xl font-bold italic">Welcome</p>
+      </div>
+      <div>
+        <p className="text-3xl w-[800px] tracking-[.1rem] leading-[4rem] italic">
+          Paper + Land is an art and design studio based in Leeds, creating
+          contemporary landscape art prints, cards and homewares. We combine
+          Indian ink with digital methods to create joyful, vibrant artwork that
+          feeds your soul.
+        </p>
+      </div>
+      {/* </div> */}
+    </div>
+  );
+};
+
+export default Section2;
