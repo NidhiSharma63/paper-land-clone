e73c5e182bb8fb61e8c4d49f46114d9c4b9bfc39
nidhisharma63 <nidhisharma639593@gmail.com>
2024-06-02 12:14:14 +0530

    animation change of section-2

--- src/components/Section2.jsx
+++ src/components/Section2.jsx
@@ -1,32 +1,89 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
+
 const Section2 = () => {
+  useGSAP(() => {
+    // apply parallax effect to any element with a data-speed attribute
+    gsap.from(".section-2-text span", {
+      opacity: 0,
+      y: 10, // Increased duration
+      ease: "ease.in",
+      stagger: 0.1,
+      scrollTrigger: {
+        start: "top 80%",
+        end: "bottom bottom",
+        scrub: 2, // Increased scrub duration,
+        markers: true,
+        trigger: ".section-2",
+      },
+    });
+  });
   return (
     <div
       data-scroll
       data-scroll-speed="0"
-      className="h-[700px] flex border-2 bg-[#fccff6] text-[#373637] align-middle justify-between p-[4rem] pt-[8rem]"
+      className="section-2 h-[700px] flex border-2 bg-[#fccff6] text-[#373637] align-middle justify-between p-[4rem] pt-[8rem]"
     >
       {/* <div className="w-[80%] h-[80%] m-auto border-2 flex gap-24"> */}
       <div className="">
         <p
-          className="text-4xl font-bold italic"
+          className="text-4xl font-bold italic section-2-text"
           data-scroll
           data-scroll-speed="-2"
           direction="vertical"
         >
-          Welcome
+          <span className="inline-block">W</span>
+          <span className="inline-block">E</span>
+          <span className="inline-block">L</span>
+          <span className="inline-block">C</span>
+          <span className="inline-block">O</span>
+          <span className="inline-block">M</span>
+          <span className="inline-block">E</span>
         </p>
       </div>
       <div>
         <p
-          data-scroll
-          data-scroll-speed="2"
+          // data-scroll
+          // data-scroll-speed="2"
           // direction="vertical"
-          className="text-3xl w-[800px] tracking-[.1rem] leading-[4rem] italic"
+          className="section-2-text text-3xl w-[800px] tracking-[.1rem] leading-[4rem] italic"
         >
-          Paper + Land is an art and design studio based in Leeds, creating
-          contemporary landscape art prints, cards and homewares. We combine
-          Indian ink with digital methods to create joyful, vibrant artwork that
-          feeds your soul.
+          <span className="inline-block">Paper</span>&nbsp;
+          <span className="inline-block">+</span>&nbsp;
+          <span className="inline-block">Land</span>&nbsp;
+          <span className="inline-block">is</span>&nbsp;
+          <span className="inline-block">an</span>&nbsp;
+          <span className="inline-block">art</span>&nbsp;
+          <span className="inline-block">and</span>&nbsp;
+          <span className="inline-block">design</span>&nbsp;
+          <span className="inline-block">studio</span>&nbsp;
+          <span className="inline-block">based</span>&nbsp;
+          <span className="inline-block">in</span>&nbsp;
+          <span className="inline-block">Leeds,</span>&nbsp;
+          <span className="inline-block">creating</span>&nbsp;
+          <span className="inline-block">contemporary</span>&nbsp;
+          <span className="inline-block">landscape</span>&nbsp;
+          <span className="inline-block">art</span>&nbsp;
+          <span className="inline-block">prints,</span>&nbsp;
+          <span className="inline-block">cards</span>&nbsp;
+          <span className="inline-block">and</span>&nbsp;
+          <span className="inline-block">homewares.</span>&nbsp;
+          <span className="inline-block">We</span>&nbsp;
+          <span className="inline-block">combine</span>&nbsp;
+          <span className="inline-block">Indian</span>&nbsp;
+          <span className="inline-block">ink</span>&nbsp;
+          <span className="inline-block">with</span>&nbsp;
+          <span className="inline-block">digital</span>&nbsp;
+          <span className="inline-block">methods</span>&nbsp;
+          <span className="inline-block">to</span>&nbsp;
+          <span className="inline-block">create</span>&nbsp;
+          <span className="inline-block">joyful,</span>&nbsp;
+          <span className="inline-block">vibrant</span>&nbsp;
+          <span className="inline-block">artwork</span>&nbsp;
+          <span className="inline-block">that</span>&nbsp;
+          <span className="inline-block">feeds</span>&nbsp;
+          <span className="inline-block">your</span>&nbsp;
+          <span className="inline-block">soul.</span>&nbsp;
         </p>
       </div>
       {/* </div> */}
@@ -35,3 +92,7 @@ const Section2 = () => {
 };
 
 export default Section2;
+// aper + Land is an art and design studio based in Leeds, creating
+//           contemporary landscape art prints, cards and homewares. We combine
+//           Indian ink with digital methods to create joyful, vibrant artwork that
+//           feeds your soul.
