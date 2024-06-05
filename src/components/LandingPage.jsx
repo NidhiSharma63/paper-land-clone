8e0ba2721af51db0c75f464f94aa23601c727eac
nidhisharma63 <nidhisharma639593@gmail.com>
2024-06-05 14:04:21 +0530

    landing page responsiveness added

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,9 +1,13 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import _ScrollTrigger from "gsap/ScrollTrigger";
+import { useCallback, useRef, useState } from "react";
 gsap.registerPlugin(_ScrollTrigger);
 
 const LandingPage = () => {
+  const cursorExampleRef = useRef(null);
+  const logoRef = useRef(null);
+  const [mouseEnter, setMouseEnter] = useState(false);
   // add text animation using gsap
 
   useGSAP(() => {
@@ -51,14 +55,58 @@ const LandingPage = () => {
         end: "max",
         invalidateOnRefresh: true,
         scrub: 2, // Increased scrub duration
-        markers: true,
+        // markers: true,
         trigger: ".smooth-scroll",
       },
     });
+
+    // logoRef.current?.forEach((link) => {
   });
 
+  // useEffect(() => {
+  //   if (!logoRef.current) {
+  //     return;
+  //   }
+
+  //   const handleMouseEnter = () => {
+  //     cursorExampleRef.current.classList.add("grow");
+  //   };
+
+  //   const handleMouseLeave = () => {
+  //     // console.log("mouse out");
+  //     cursorExampleRef.current.classList.remove("grow");
+  //     cursorExampleRef.current.classList.remove("grow-small");
+  //   };
+
+  //   const newRef = logoRef.current;
+  //   newRef.addEventListener("mousemove", handleMouseEnter);
+  //   newRef.addEventListener("mouseleave", handleMouseLeave); // Use mouseleave instead of mouseout
+
+  //   newRef.addEventListener("mouseenter", () => console.log("Mouse Enter"));
+  //   newRef.addEventListener("mouseover", () => console.log("Mouse Over"));
+  //   // newRef.addEventListener("mouseleave", () => console.log("Mouse Leave"));
+  //   // newRef.addEventListener("mouseout", () => console.log("Mouse Out"));
+  //   return () => {
+  //     newRef.removeEventListener("mousemove", handleMouseEnter);
+  //     newRef.removeEventListener("mouseleave", handleMouseLeave);
+  //   };
+  // }, []);
+
+  const handleMouseEnter = useCallback(() => {
+    // cursor.classList.add('grow');
+    // if
+    cursorExampleRef.current.classList.add("grow");
+  }, []);
+
+  const handleMouseLeave = () => {
+    // console.log("mouse out");
+    cursorExampleRef.current.classList.remove("grow");
+    cursorExampleRef.current.classList.remove("grow-small");
+  };
   return (
     <div data-scroll data-scroll-speed="-3">
+      <div className="cursor" />
+      <div className="cursor-example" ref={cursorExampleRef} />
       <div className="h-screen bg-black flex items-center justify-center">
         <h1 className="logo text-7xl text-white uppercase font-bold flex gap-2">
           <span className="block">P</span>
@@ -76,21 +124,35 @@ const LandingPage = () => {
       <div className="pinkish-div absolute bottom-0 z-1 bg-[#D6BAD0] w-full" />
       <div className="greenish-div absolute w-0 top-0 right-0 z-2 bg-[#16a991] h-full" />
       <div className="main-div absolute w-full h-full top-0 right-0 z-3 flex justify-between">
-        <header className="header flex w-full justify-between text-white p-11 align-middle">
-          <h1 className="text-2xl uppercase font-extrabold tracking-[3px]">
+        <header className="header flex w-full justify-between align-middle text-white md:p-11 p-4">
+          {/* <div
+            ref={logoRef}
+            className="flex align-middle justify-center w-[250px] h-[50px]"
+            // onMouseEnter={() => {
+            //   console.log("mouseEnter");
+            // }}
+            onMouseMove={() => {
+              console.log("mouseMove");
+            }}
+            onMouseLeave={() => {
+              console.log("mouseLeave");
+            }}
+          > */}
+          <h1 className="md:text-2xl text-lg uppercase font-extrabold md:tracking-[3px] tracking-[1.8px]">
             Paper + Land
           </h1>
-          <div className="flex gap-12 text-[#272624da] bg-[#cdc5bfa0] align-middle justify-between px-12 py-6 text-2xl rounded-full">
+          {/* </div> */}
+          <div className="flex md:gap-12 gap-4 text-[#272624da] bg-[#cdc5bfa0] align-middle justify-between md:px-12 md:py-6 px-6 py-3 md:text-2xl text-lg rounded-full">
             <i className="fa-solid fa-cart-shopping" />
-            <div className="flex gap-3">
+            <div className="flex items-center justify-between md:gap-3 gap-1">
               <i className="fa-solid fa-magnifying-glass" />
               <i className="fa-solid fa-bars" />
             </div>
           </div>
         </header>
         <div
-          className="text-white text-[5rem] mt-20 px-11 text-center 
-       w-[85%] m-auto text-wrap font-extrabold tracking-[3px] flha"
+          className="text-white  lg:text-[5rem] md:text-[4rem] sm:text-[3rem] text-[2.5rem] mt-20 px-11 text-center 
+       w-[85%] m-auto text-wrap font-extrabold tracking-[3px] "
         >
           <h1 className="main-text relative top-[2rem] capitalize flex w-full flex-wrap m-auto text-center justify-center">
             <div className="flex">
@@ -123,7 +185,6 @@ const LandingPage = () => {
                 Y
               </span>
             </div>
-            &nbsp;
             <div className="flex">
               <span data-speed="1.211" className="block">
                 U
@@ -153,7 +214,6 @@ const LandingPage = () => {
                 G
               </span>
             </div>
-            &nbsp;
             <div className="flex">
               <span data-speed="1.277" className="block">
                 L
