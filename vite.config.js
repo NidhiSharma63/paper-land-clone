64383bf3e5b6398470b759aa6e1545e2d1621ccf
nidhisharma63 <nidhisharma639593@gmail.com>
2024-06-08 14:08:34 +0530

    packge.json reverted bacj

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,14 @@ export default defineConfig({
       plugins: [tailwindcss()],
     },
   },
-  build: {
-    outDir: "dist", // This is the default, change it to 'build' if you prefer
-  },
+  // build: {
+  //   outDir: "dist", // This is the default, change it to 'build' if you prefer
+  // },
 });
+
+ // "dev": "vite",
+    // "build": "vite build",
+    // "lint": "eslint . --ext js,jsx --report-unused-disable-directives --max-warnings 0",
+    // "preview": "vite preview",
+    // "deploy": "gh-pages -d dist",
+    // "predeploy": "npm run build"
