e91da0b6b42e0e8e494094692abecde983d9867f
nidhisharma63 <nidhisharma639593@gmail.com>
2024-06-10 09:57:55 +0530

    section-5 responsivness added

--- src/components/Section5.jsx
+++ src/components/Section5.jsx
@@ -28,11 +28,11 @@ const data = [
 const Section5 = React.forwardRef((_, ref) => {
   useGSAP(() => {
     gsap.from(".section-5-card-image", {
-      scale: 0.7,
+      // scale: 0.7,
       opacity: 0,
       duration: 1,
       scrollTrigger: {
-        start: "top 20%",
+        start: "top 70%",
         end: "bottom 70%",
         invalidateOnRefresh: true,
         scrub: 1,
@@ -59,19 +59,21 @@ const Section5 = React.forwardRef((_, ref) => {
     ref.current?.classList.remove("section-3-cursor-animation");
   }, [ref]);
   return (
-    <div className="section-5 max-w-[1440px] m-auto h-auto flex  bg-[#fccff6] text-[#373637] align-middle justify-between md:p-[4rem] pt-[4rem] p-[1rem] pl-[1rem] md:pl-[7rem] flex-col gap-16">
+    <div className="section-5 max-w-[1920px] m-auto h-auto flex  bg-[#fccff6] text-[#373637] align-middle justify-between md:p-[4rem] pt-[4rem] p-[1rem] pl-[1rem] md:pl-[7rem] flex-col gap-16">
       <div className="flex align-middle justify-start gap-12">
-        <h3 className="text-2xl font-bold heading">More Products</h3>
+        <h3 className="md:text-2xl text-sm 2xl:text-3xl 3xl:text-4xl font-bold heading">
+          More Products
+        </h3>
         <button
           onMouseEnter={handleMouseEnterCursor}
           onMouseLeave={handleMouseLeaveCursor}
-          className="border-2 px-4 py-1 rounded text-[11px] font-medium tracking-wide border-[#181816cc]"
+          className="md:text-[0.7rem] text-[0.5rem] 2xl:text-lg 3xl:text-xl border-2 px-4 py-1 rounded text-[11px] font-medium tracking-wide border-[#181816cc] hover:bg-[#181816ee] hover:text-white transition-all duration-500"
         >
-          VIEW ALL
+          VIEW ALL PRODUCTS
         </button>
       </div>
 
-      <div className="image-grid grid md:grid-cols-4 grid-cols-2 gap-2 ">
+      <div className="image-grid grid md:grid-cols-4 grid-cols-2 gap-8">
         {data.map((elem) => {
           return (
             <div
@@ -80,7 +82,7 @@ const Section5 = React.forwardRef((_, ref) => {
             >
               <div className="w-full rounded-xl overflow-hidden">
                 <img
-                  className="w-full h-full transition-transform duration-300 ease-out group-hover:scale-110  section-5-card-image"
+                  className="w-full h-full transition-transform duration-300 ease-out group-hover:scale-110  section-5-card-image object-cover"
                   src={elem.image}
                   alt={elem.heading1}
                   onMouseEnter={handleMouseEnter}
@@ -88,11 +90,11 @@ const Section5 = React.forwardRef((_, ref) => {
                 />
               </div>
               <div className="flex flex-col align-middle text-center">
-                <p className="md:text-[22px] text-[15px] font-bold transition-transform duration-300 ease-out group-hover:scale-125">
+                <p className="md:text-[22px] text-[15px] 2xl:text-[26px] 3xl:text-[30px] font-bold transition-transform duration-300 ease-out group-hover:scale-125">
                   {elem.heading1}
                 </p>
-                <p>Skipton Art Place</p>
-                <p className="md:text-[20px] text-[15px] md:mt-2 mt-1">
+                <p className="md:text-[20px] text-[15px]">Skipton Art Place</p>
+                <p className="md:text-[20px] text-[15px] 2xl:text-[26px] 3xl:text-[30px]  md:mt-2 mt-1">
                   £24.00 – £42.00
                 </p>
               </div>
